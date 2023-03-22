9b5013e9e925bbc1c0898a2a87f27df87eb4486b
Duc Viet Truong <truongvietduc@gmail.com>
2023-03-23 02:57:25 +0700

    Self-effort to sample code Dijkstra

---
+++ Heap/ICanGuessTheDataStructure/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int caseCount = 0;
+    while (caseCount < 2*n) {
+        
+    }
+    return 0;
+}
